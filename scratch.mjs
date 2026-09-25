b2e3e5d66bf59e8a9e44ee2c79c87649b12c4167
juoneindia <juone.india@gmail.com>
2026-09-25 23:34:50 +0530

    Update countdown target date in scratch.mjs

--- scratch.mjs
+++ scratch.mjs
@@ -4,7 +4,7 @@ import FlipClockCountdown from '@leenguyen/react-flip-clock-countdown';
 
 console.log('--- START ---');
 try {
-  const html = renderToString(React.createElement(FlipClockCountdown, { to: new Date('2026-10-15T23:59:59+05:30').getTime(), renderOnServer: true }));
+  const html = renderToString(React.createElement(FlipClockCountdown, { to: new Date().getTime()+ 10000, renderOnServer: true }));
   console.log('HTML:', html);
 } catch (err) {
   console.error('ERROR:', err);
